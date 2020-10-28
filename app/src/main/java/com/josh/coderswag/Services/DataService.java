6a55c3af6cb9d4914ba3769a1ccbe21c879572b0
Zodraz <el_josho@hotmal.com>
2020-10-28 23:56:50 +1100

    added DataService and data

---
+++ app/src/main/java/com/josh/coderswag/Services/DataService.java
@@ -0,0 +1,50 @@
+package com.josh.coderswag.Services;
+
+import com.josh.coderswag.Model.Category;
+import com.josh.coderswag.Model.Product;
+
+import java.util.List;
+
+public class DataService {
+
+    private static DataService dataService;
+
+    private final List<Category> categories = List.of(
+            new Category("SHIRTS", "shirtimage"),
+            new Category("HOODIES", "hoodieimage"),
+            new Category("HATS", "hatimage"),
+            new Category("DIGITAL", "digitalgoodsimage")
+    );
+
+    private final List<Product> hats = List.<Product>of(
+            new Product("Devslopes Graphic Beanie", "$18", "hat01"),
+            new Product("Devslopes Hat Black", "$20", "hat02"),
+            new Product("Devslopes Hat White", "$18", "hat03"),
+            new Product("Devslopes Hat Snapback", "22", "hat04")
+    );
+
+    private final List<Product> hoodies = List.<Product>of(
+            new Product("Devslopes Hoodie Gray", "$18", "hoodie01"),
+            new Product("Devslopes Hoodie Red", "$32", "hoodie02"),
+            new Product("Devslopes Hoodie Blue", "$18", "hoodie03"),
+            new Product("Devslopes Hoodie Black", "$32", "hoodie04")
+    );
+
+    private final List<Product> shirts = List.<Product>of(
+            new Product("Devslopes Shirt Gray", "$18", "shirt01"),
+            new Product("Devslopes Logo Shirt", "$32", "shirt02"),
+            new Product("Devslopes Shirt Blue", "$18", "shirt03"),
+            new Product("Devslopes Hustle", "$32", "shirt04"),
+            new Product("Kickflip Studios", "$18", "shirt05")
+    );
+
+    private DataService() {
+    }
+
+    public static DataService getDataService() {
+        if (dataService == null){
+            dataService = new DataService();
+        }
+        return dataService;
+    }
+}
